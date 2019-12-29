d290a306a0658e371bd73363b6a30387c79182a7
Joel Longanecker <joel.longanecker@gmail.com>
2019-12-28 18:17:53 -0600

    Update main.lua

--- src/scripts/main.lua
+++ src/scripts/main.lua
@@ -4,7 +4,8 @@ while true do
  
     local mouse = PollMouse()
     ClearBuffer()
-    DrawString(mouse.x, mouse.y, "Age of Chaos!")
+    DrawString(mouse.x, mouse.y, ">")
+    DrawString(mouse.x+8, mouse.y, "<")
 
     SwapBuffers()
 
