281aa5b3ce79715b73c964e2840444ffe20f169f
Joel Longanecker <joel.longanecker@gmail.com>
2020-01-05 21:28:18 -0600

    we're cooking now

--- src/scripts/main.lua
+++ src/scripts/main.lua
@@ -1,18 +1,27 @@
 -- main
-local TitleScreen = require("titlescreen.lua")
+
+local ReadAll = function(file)
+    local f = assert(io.open(file, "rb"))
+    local content = f:read("*all")
+    f:close()
+    return content
+end
+
+local TitleScreen = load(ReadAll("scripts/tscrn.lua"))()
+TitleScreen.onLoad()
 
 local state = {}
 
 local stateStack = {TitleScreen}
 
-for v,_ in stateStack do
-    v.onLoad()
-end
 
-while table.getn(stateStack) > 0 do
-    local top = stateStack(#stateStack)
+
+while #stateStack > 0 do
+    local top = stateStack[#stateStack]
 
     top.onFrame(state)
 
     top.onRender(state)
+
+    SwapBuffers()
 end
